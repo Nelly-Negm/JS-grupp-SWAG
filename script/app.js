a729895c864743c8133856e96f862ae79d6b1040
Nelly-Negm <negmnelly@gmailcom>
2021-11-04 17:49:19 +0100

    INserted the new JS i want to make work

--- script/app.js
+++ script/app.js
@@ -5,3 +5,26 @@ function myFunction(x) {
 
 //card 
 
+const backSideInformation = [ //personal information about each card
+    {
+        name: "",
+    age: "",
+    skill: "",
+    like: "",
+    background: "",
+    }
+    
+]
+
+function card-flip() { //making it flip
+    let element = document.getElementById("front"); //first it is on front
+    element.toggle.getElementById("back") //use the toogle event to flipp it
+}
+
+object.onclick = function(card-flip){ //i want onclik to be the indicator for it to flipp
+    let back = document.getElementById("back");
+    let front = document.getElementById("front");
+
+    //can I use the same to rotade ass in CSS? like:
+    //https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_animate_3
+};
