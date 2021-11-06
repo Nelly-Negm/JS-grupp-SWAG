fc9630c3eb0d449a67eb77bd523e63b4f60d6a3d
Nelly-Negm <negmnelly@gmailcom>
2021-11-06 14:59:42 +0100

    trying to fix the cards. they wont move

--- script/main.js
+++ script/main.js
@@ -2,46 +2,37 @@
     //https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_animate_3
 
     //Movement Animation to happen
-const card = document.querySelector(".desktop-card-inner");
-const container = document.querySelector(".desktop-card");
-
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
-const profile = document.querySelector(".card-top img");
-const description = document.querySelector(".info ");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
 
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  let xAxis = (window.innerWidth / 1 - e.pageX) / 3;
+  let yAxis = (window.innerHeight / 1 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
-
-  //Animate In
+//Animate In
 container.addEventListener("mouseenter", (e) => {
-    card.style.transition = "none";
-
-//Popout
-    title.style.transform = "translateZ(150px)";
-
-    profile.style.transform = "translateZ(200px) rotateZ(-45deg)";
-
-    description.style.transform = "translateZ(125px)";
-
+  card.style.transition = "all 0.3s ease";
+  //Popout
+ /* */ title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-10deg)";
+  description.style.transform = "translateZ(125px)";
+  purchase.style.transform = "translateZ(75px)";
 });
-
-// animate out
-
-container.addEventListener("moueleave", (e) => {
-card.style.transition = "all 0.5s ease";
-card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-//Popback
-
-title.style.transform = "translateZ(0px)";
-
-    profile.style.transform = "translateZ(0px) rotateZ(0deg)";
-
-    description.style.transform = "translateZ(0px)";
-
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 1.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  /* */ title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
 });
