a1a06d6d13d00d312926c7dc3ea173ddd2072af4
Nelly-Negm <negmnelly@gmailcom>
2021-11-09 09:41:45 +0100

    comentar

--- script/main.js
+++ script/main.js
@@ -2,30 +2,31 @@
 //https://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_animate_3
 
 //Movement Animation to happen
-const cards = document.querySelectorAll(".card");
-const container = document.querySelector(".container");
+const cards = document.querySelectorAll(".card"); //targeting all the cards
+const container = document.querySelector(".container"); //what on/in the cards
 //Items
 
-const profile = document.querySelector(".profile img");
+const profile = document.querySelector(".profile img"); //target the image to make it rotate
 
 //Moving Animation Event
-cards.forEach(card => {
-  card.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 1 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 1 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-  });
-  card.addEventListener("mouseenter", (e) => {
-    card.style.transition = "none";
-    //Popout
-    profile.style.transform = "translateZ(200px) rotateZ(-45deg)";
-  });
-  card.addEventListener("mouseleave", (e) => {
-    card.style.transition = "all 0.5s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    //Popback
-    profile.style.transform = "translateZ(0px) rotateZ(0deg)";
-
-  });
+cards.forEach((card) => {
+    card.addEventListener("mousemove", (e) => {
+        //traking the mouse to rotate the card in different ddirections
+        let xAxis = (window.innerWidth / 1 - e.pageX) / 25;
+        let yAxis = (window.innerHeight / 1 - e.pageY) / 25;
+        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`; // y and X axel is horesental and vertikal
+    });
+    card.addEventListener("mouseenter", (e) => {
+        //mouseenter is so it wont do anything wierd when on the card
+        card.style.transition = "none";
+        //Popout
+        profile.style.transform = "translateZ(200px) rotateZ(-45deg)"; //-45degre helps the angle with the 3d effect
+    });
+    card.addEventListener("mouseleave", (e) => {
+        // when mouse leave card, there is a 0.5s ease so i will look smother
+        card.style.transition = "all 0.5s ease";
+        card.style.transform = `rotateY(0deg) rotateX(0deg)`; //and it goes back to standarrt card
+        //Popback
+        profile.style.transform = "translateZ(0px) rotateZ(0deg)";
+    });
 });
-
